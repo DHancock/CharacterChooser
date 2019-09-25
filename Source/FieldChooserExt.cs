24b5bd3516ba301c43e4e0c51f41d02af5ddb1ee
DHancock <code@davidhancock.net>
2019-09-25 20:58:28 +0100

    Add license preamble
    Add gpl license to file header.
    Small code refactor.

--- Source/FieldChooserExt.cs
+++ Source/FieldChooserExt.cs
@@ -1,7 +1,25 @@
-﻿using System;
+﻿/*
+Copyright 2019 David Hancock
+
+This file is part of the FieldChooser plugin for KeePass 2.
+
+FieldChooser is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+FieldChooser is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FieldChooser.  If not, see<https://www.gnu.org/licenses/>.
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
-using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Windows.Forms;
 
