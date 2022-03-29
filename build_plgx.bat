7d7e99c8ade8fedffb3131a65a34a963cd1e00e2
DHancock <code@davidhancock.net>
2022-03-29 13:36:53 +0100

    Revert "Update project to VS 2022, removing plgx build"
    This reverts commit c5c2c4a7680452047a92e72503bf9e938b6db779.

---
+++ build_plgx.bat
@@ -0,0 +1,34 @@
+rem @echo off
+
+echo Starting post build batch file: %~nx0
+echo.
+
+setlocal enableextensions
+
+set TargetName=%~1
+set TargetPath=%~2
+set ProjectName=%~3
+set ProjectPath=%~4
+set SrcDir=%ProjectPath%plgx\%TargetName%
+
+echo TargetPath  = %TargetPath%
+echo ProjectPath = %ProjectPath%
+echo SrcDir      = %SrcDir%
+echo.
+
+rem The plgx file name will be the same as the name of the directory containing the source
+if exist "%SrcDir%.plgx" del /q /f "%SrcDir%.plgx"
+if exist "%SrcDir%" rd /s /q "%SrcDir%"
+
+md "%SrcDir%"
+
+copy /y "%ProjectPath%%ProjectName%.csproj" "%SrcDir%\%ProjectName%.csproj"
+xcopy /y /s /i "%ProjectPath%Source" "%SrcDir%\Source"
+xcopy /y /s /i "%ProjectPath%Resources" "%SrcDir%\Resources"
+xcopy /y /s /i "%ProjectPath%Properties" "%SrcDir%\Properties"
+ 
+start /wait "" "%TargetPath%KeePass.exe" --plgx-create --plgx-prereq-net:4.0 --plgx-prereq-kp:2.41 "%SrcDir%"
+
+rd /s /q "%SrcDir%"
+
+echo finished...
